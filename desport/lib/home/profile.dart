e003bbe0bf20ed458e130dee09516afe3b191137
Eduardo Agostinetti <eduardo.cardoso@edu.unifil.br>
2024-10-26 20:00:06 -0300

    New

--- desport/lib/home/profile.dart
+++ desport/lib/home/profile.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'dart:io';
 
 class Profile extends StatefulWidget {
   final Map<String, dynamic> user;
--- desport/lib/settings/language.dart
+++ desport/lib/settings/language.dart
@@ -1,4 +1,3 @@
-import 'package:desport/auth/signin.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:easy_localization/easy_localization.dart';
